8aff9cbcdf1e39cccae20590c2fecfde667d0354
Madan Gangwar <madanji.official@gmail.com>
2022-08-22 16:56:43 +0530

    added

---
+++ src/src/collections/StackTest.java
@@ -0,0 +1,28 @@
+package collections;
+import  java.util.Iterator;
+import java.util.Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("Madan");
+        stack.push("Karan");
+        stack.push("Arun");
+        stack.push("Mohit");
+        stack.push("Ram");
+        // Iterate by Iterator
+        Iterator iterator = stack.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        //stack.pop();
+        System.out.println(stack);
+        // Iterate by Iterator
+        for (String str : stack) {
+            System.out.println(str);
+        }
+        for (int i = 0; i < stack.size();i++){
+            System.out.println(stack.get(i));
+        }
+    }
+}
