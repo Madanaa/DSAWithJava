ec27c0266fda46e84828db812974593052af77d1
Madan Gangwar <madanji.official@gmail.com>
2022-08-22 09:19:41 +0530

    added

---
+++ src/src/collections/LinkedListTest.java
@@ -0,0 +1,36 @@
+package collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+// implement linkedList
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<String> li = new LinkedList<>();
+        li.add("Ajay");
+        li.add("Arjun");
+        li.add("Ram JI");
+        li.add("Seeta Ram");
+        // list print
+        System.out.println(li);
+        // return last element by getLast() method
+        System.out.println(li.getLast());
+        // return fast element by getLast() method
+        System.out.println(li.getFirst());
+        // return element by index with get() method
+        System.out.println(li.get(2));
+        // iterate by Iterator
+        Iterator<String> it = li.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        // Retrieve by for each loop
+        for ( String str : li) {
+            System.out.println(str);
+        }
+        // Retrieve by for loop
+        for (int i = 0; i < li.size(); i++) {
+            System.out.println(li.get(i));
+        }
+    }
+
+}
