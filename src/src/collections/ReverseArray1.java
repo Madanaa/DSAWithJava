52a9fb1b9965b0347c55f7808672dac315e38796
madanaa <madandev1994@gmail.com>
2022-09-17 00:31:54 +0530

    added

---
+++ src/src/collections/ReverseArray1.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.Scanner;
+
+public class ReverseArray1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        for (int i = 0 ; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+        for (int i = n-1; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+    }
+}
