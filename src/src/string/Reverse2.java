8d742ddae25331ab0203d195f141001f858dc34c
madanaa <madandev1994@gmail.com>
2022-10-06 20:53:29 +0530

    added

---
+++ src/src/string/Reverse2.java
@@ -0,0 +1,9 @@
+package string;
+
+public class Reverse2 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
+    }
+}
