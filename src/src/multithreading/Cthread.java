c2137511ade19053b1dc19d0a157e97550ab47a3
madanaa <madandev1994@gmail.com>
2022-10-07 12:49:58 +0530

    added

---
+++ src/src/multithreading/Cthread.java
@@ -0,0 +1,16 @@
+package multithreading;
+
+public class Cthread extends Thread{
+    static int amount = 0;
+    public void run() {
+        amount++;
+    }
+
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        t.start();
+        System.out.println(amount);
+        amount++;
+        System.out.println(amount);
+    }
+}
