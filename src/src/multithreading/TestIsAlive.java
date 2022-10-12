4c7c59c8623224c4e1dd43a1533bceae373de5ae
madanaa <madandev1994@gmail.com>
2022-10-13 00:17:33 +0530

    added

---
+++ src/src/multithreading/TestIsAlive.java
@@ -0,0 +1,22 @@
+package multithreading;
+
+public class TestIsAlive extends Thread{
+    public void run() {
+        int i =1;
+        while (i < 5) {
+            System.out.println("Thread name = "+Thread.currentThread().getName());
+            i++;
+        }
+    }
+}
+class Test2 {
+    public static void main(String[] args) {
+        TestIsAlive t = new TestIsAlive();
+        TestIsAlive t1 = new TestIsAlive();
+        System.out.println(t.isAlive());
+        t.start();
+        System.out.println("*******************");
+        t1.start();
+        System.out.println(t1.isAlive());
+    }
+}
