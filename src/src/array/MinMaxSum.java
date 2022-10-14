01fc279014733c434da17d6de3cbb48f76d46e89
madanaa <madandev1994@gmail.com>
2022-10-14 09:15:13 +0530

    added

---
+++ src/src/array/MinMaxSum.java
@@ -0,0 +1,24 @@
+package array;
+
+public class MinMaxSum {
+
+    public static int findSum(int a[] , int n) {
+        int max = a[0];
+        int min = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
+        return  max + min;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1,4,8,7};
+        int n = a.length;
+        System.out.println(findSum(a,n));
+    }
+}
