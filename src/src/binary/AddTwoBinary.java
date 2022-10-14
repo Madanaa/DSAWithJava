8545657abd54506eb09f70ea1bbaceba389dbf3e
madanaa <madandev1994@gmail.com>
2022-10-15 02:30:22 +0530

    added

---
+++ src/src/binary/AddTwoBinary.java
@@ -0,0 +1,25 @@
+package binary;
+
+public class AddTwoBinary {
+    static String addBinary(String x, String y) {
+        String res = "";
+        int sum = 0;
+        // traverse string starting to last
+        int t = x.length()-1,  t1 = y.length()-1;
+        while (t >= 0 || t1 >= 0 || sum == 1) {
+            sum += ((t >= 0) ? x.charAt(t) - '0' : '0');
+            sum += ((t1 >= 0) ? y.charAt(t1) - '0' : '0');
+            res = (char)(sum % 2 + '0') + res;
+            sum /= 2;
+            t--;
+            t1--;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String x = "110";
+        String y = "011";
+        System.out.println(addBinary(x,y));
+    }
+}
