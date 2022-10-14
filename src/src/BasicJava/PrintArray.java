d56c4fb60bf65ffe784b39650770b2abbe17785c
madanaa <madandev1994@gmail.com>
2022-10-15 00:02:15 +0530

    added

---
+++ src/src/BasicJava/PrintArray.java
@@ -0,0 +1,11 @@
+package BasicJava;
+
+public class PrintArray {
+    static void printAsscii(char c) {
+        int value = (int) c;
+        System.out.println(value);
+    }
+    public static void main(String[] args) {
+        printAsscii('a');
+    }
+}
