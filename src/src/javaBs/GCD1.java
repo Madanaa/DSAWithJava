aa466874f020d13c8a9d3abddd35ebb428969ca4
madanaa <madandev1994@gmail.com>
2022-10-19 19:26:05 +0530

    added

---
+++ src/src/javaBs/GCD1.java
@@ -0,0 +1,13 @@
+package javaBs;
+
+public class GCD1 {
+    static int gcd(int x, int y) {
+        if (y == 0)
+            return x;
+        return gcd(y,x % y );
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(10,20));
+    }
+}
---
+++ src/src/javaBs/GreatestCommonDivider.java
@@ -0,0 +1,19 @@
+package javaBs;
+
+public class GreatestCommonDivider {
+    static int gcd(int x, int y) {
+        if (x == 0)
+            return y;
+        if (y == 0)
+            return x;
+        if (x == y)
+            return  x;
+        if (x > y)
+            return gcd(x-y,y);
+        return gcd(x,y-x);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(10,20));
+    }
+}
