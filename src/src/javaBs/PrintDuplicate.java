03428ae068c3e6d926e9623ad4e4211193898a12
madanaa <madandev1994@gmail.com>
2022-11-10 04:56:23 +0530

    added

---
+++ src/src/javaBs/PrintDuplicate.java
@@ -0,0 +1,14 @@
+package javaBs;
+
+public class PrintDuplicate {
+    public static void main(String[] args) {
+        int a[] = {1,6,8,4,8,4};
+        for (int i = 0; i <a.length;i++) {
+            for (int j = i +1; j < a.length; j++) {
+                if (a[i] == a[j]){
+                    System.out.println(a[i]);
+                }
+            }
+        }
+    }
+}
