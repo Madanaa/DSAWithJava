51a8a9264701053255e67668dea1cd9bdbb88428
madanaa <madandev1994@gmail.com>
2022-11-11 03:53:53 +0530

    added

---
+++ src/src/javaBs/FindPerfectNumber.java
@@ -0,0 +1,21 @@
+package javaBs;
+
+public class FindPerfectNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+        if (n > 0) {
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sum = sum + i;
+                }
+            }
+            if (n == sum) {
+                System.out.println("Perfect number");
+            }
+            else {
+                System.out.println("Not a perfect number");
+            }
+        }
+    }
+}
